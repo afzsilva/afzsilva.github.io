63edb64dd99be4b2dea2230748f3f59f7049e6b0
afzsilva <afranio26@yahoo.com.br>
2018-10-18 10:56:31 -0300

    Alteração no JS

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-//window.addEventListener('DOMContentLoaded',function(e){
-//    document.querySelector('.hb-btn').addEventListener('click',function (e){
-//    document.querySelector('.m').classList.toggle('.n');
-//    },false);
-//},false);
-
-document.getElementsByClassName('hb-btn')[0].addEventListener('click',e=>{
-            document.getElementsByClassName('m')[0].classList.toggle('n');
+document.getElementsByClassName('btn-mob')[0].addEventListener('click',e=>{
+            document.getElementsByClassName('menu-mob')[0].classList.toggle('menu-mob-hide');
         });
