eb2f377b2acbdc32948953351fb2f56013b2a4ea
Afranio <afzsilva@gmail.com>
2020-09-21 19:34:43 -0300

    Atualização de pagina

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
-document.getElementsByClassName('btn-mob')[0].addEventListener('click',e=>{
-            document.getElementsByClassName('menu-mob')[0].classList.toggle('menu-mob-hide');
-        });
+let header = document.querySelector('#header');
+let hamburgerMenu = document.querySelector('.hamburger-menu');
+
+hamburgerMenu.addEventListener('click', function () {
+    header.classList.toggle('menu-open');
+
+});
+
+let hero = document.querySelector('a[href="#hero"]');
+let habilidades = document.querySelector('a[href="#habilidades"]');
+let projetos = document.querySelector('a[href="#projetos"]');
+let contato = document.querySelector('a[href="#contato"]');
+
+
+hero.addEventListener('click', function () {
+    header.classList.remove('menu-open');
+});
+habilidades.addEventListener('click', function () {
+    header.classList.remove('menu-open');
+});
+projetos.addEventListener('click', function () {
+    header.classList.remove('menu-open');
+});
+contato.addEventListener('click', function () {
+    header.classList.remove('menu-open');
+});
